77963ee9f93aa5ffcd175c8cb77f4fa2c19fd31d
Rcallingham <rmc15@ic.ac.uk>
2022-02-13 22:12:47 +0100

    Added unit change

--- src/script.js
+++ src/script.js
@@ -10,10 +10,46 @@ let days = [
 ];
 let weekDay = days[now.getDay()];
 let hours = now.getHours();
-let minute = now.getMinutes();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let date = now.getDate();
+if (date < 2) {
+  date = `${date}st`;
+}
+if (date === 2) {
+  date = `${date}nd`;
+}
+if (date === 3) {
+  date = `${date}rd`;
+} else {
+  date = `${date}th`;
+}
+let monthName = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-let today = document.querySelector("#date");
-today.innerHTML = `${weekDay} ${hours}:${minute}`;
+let month = monthName[now.getMonth()];
+
+let todayDate = document.querySelector("#date");
+todayDate.innerHTML = `${weekDay} ${date} ${month}`;
+let lastUpdated = document.querySelector("#time");
+lastUpdated.innerHTML = `Last updated:${hours}:${minutes}`;
 
 function cityName(event) {
   event.preventDefault();
@@ -26,14 +62,39 @@ function cityName(event) {
 }
 
 function retrieveWeather(response) {
-  let h2 = document.querySelector("h2");
+  let currentTemp = document.querySelector("#temp");
   let degrees = Math.round(response.data.main.temp);
-  h2.innerHTML = `${degrees}°C`;
+  currentTemp.innerHTML = `${degrees}`;
   let h1 = document.querySelector("#displayCity");
   h1.innerHTML = response.data.name;
   let h3 = document.querySelector("h3");
   h3.innerHTML = response.data.weather[0].main;
+  let currentIcon = document.querySelector("#main-image");
+  currentIcon.setAttribute(
+    "src",
+    ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  tempCelcius = response.data.main.temp;
+}
+
+function degreesFar(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  let tempFarenheit = (tempCelcius * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(tempFarenheit);
+}
+
+function degreesCel(event) {
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(tempCelcius);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", cityName);
+
+let tempCelcius = null;
+
+let tempFar = document.querySelector("#farenheit");
+tempFar.addEventListener("click", degreesFar);
+let tempCel = document.querySelector("#celcius");
+tempCel.addEventListener("click", degreesCel);
